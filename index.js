1f57ed475bacb9f06f4731deef111d47a4c748ef
Olivia Adongo <akinyioliviatove@gmail.com>
2022-06-07 01:49:54 +0300

    commit tree 18b47593cdab4cbf3f8ce4727fe1977b41fd8054

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-var multiply
-var random
-var mod 
-var max
+var num1 =31 ;
+var num2 = 2;
+
+var multiply = num1*num2;
+
+multiply = 62;
+// multiplied
+
+var random = Math.floor((Math.random()* 10) +1);
+
+//random
+let num3 = 7;
+let num4 = 3;
+var mod = num3%num4;
+mod =4;
+
+
+let max =20;
+console.log(Math.max(5, 3, 20));
+// expected output: 3
